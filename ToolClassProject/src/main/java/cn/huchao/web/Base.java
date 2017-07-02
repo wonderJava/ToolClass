37451d8c40947549ddb3fa80ec37eb6975807470
workforfuture <18239639432@139.com>
2017-07-02 10:18:59 +0800

    创建项目

---
+++ ToolClassProject/src/main/java/cn/huchao/web/Base.java
@@ -0,0 +1,12 @@
+package cn.huchao.web;
+
+/**
+ * @author huchao
+ *	@description
+ *
+ */
+public class Base {
+	public static void main(String[] args) {
+		System.out.println("nihao,shijie");
+	}
+}
